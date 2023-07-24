702d2e63be150fd80f867ce97d591412e7ae5268
MohamedSaad71 <mohamedsaadeldin97@gmail.com>
2023-07-24 10:23:41 +0300

    Version1

---
+++ branching/branching/main.c
@@ -0,0 +1,23 @@
+/*
+ * branching.c
+ *
+ * Created: 7/24/2023 10:20:41 AM
+ * Author : pc
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+   
+   _delay_ms(500);
+    while (1) 
+    {
+    }
+}
+
